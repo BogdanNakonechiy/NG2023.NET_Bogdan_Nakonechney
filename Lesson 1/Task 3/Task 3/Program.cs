c42c6f06801287cc8b0c5b5e9fd2fd67bf672450
Bogdan Nakonechniy <nopebogdan@gmail.com>
2023-08-02 13:50:51 +0300

    number array

---
+++ Lesson 1/Task 3/Task 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = new int[10];
+
+            for(int numbCount = 0; numbCount <= numbersArr.Length - 1; numbCount++)
+            {
+                Console.Write($"Enter numb {numbCount + 1} > ");
+                int.TryParse(Console.ReadLine(), out numbersArr[numbCount]);
+            }
+
+            Console.Write("Array > ");
+            for(int numbCount = 0; numbCount <= numbersArr.Length - 1; numbCount++)
+            {
+                Console.Write(numbersArr[numbCount] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
