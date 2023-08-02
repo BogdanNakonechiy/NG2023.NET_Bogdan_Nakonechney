49045715424b067ff51e109dac1534f62cde658e
Bogdan Nakonechniy <nopebogdan@gmail.com>
2023-08-02 10:39:12 +0300

    numb + 10

---
+++ Lesson 1/Task 2/Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb;
+
+            Console.Write("Enter numb > ");
+            numb = Convert.ToInt32(Console.ReadLine()) + 10;
+
+            Console.WriteLine("Result > " + numb);
+        }
+    }
+}
