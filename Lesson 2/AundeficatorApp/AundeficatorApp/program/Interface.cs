35067209037e91a09cb972042dc38fcce36e6c4a
Bogdan Nakonechniy <nopebogdan@gmail.com>
2023-08-23 23:07:37 +0300

    interface implementation

---
+++ Lesson 2/AundeficatorApp/AundeficatorApp/program/Interface.cs
@@ -0,0 +1,42 @@
+﻿using AundeficatorApp.data;
+
+namespace AundeficatorApp.program
+{
+    internal class Interface
+    {
+        static void Main(string[] args)
+        {
+            service.reg("Admin", "123456"); // test user
+            Console.Clear();
+
+            string username;
+            string password;
+            string action;
+            
+            while (true)
+            {
+                Console.WriteLine("--- Enter data ---");
+                Console.Write("Username > ");
+                username = Console.ReadLine();
+                Console.Write("Password > ");
+                password = Console.ReadLine();
+
+                if(Account.Aundeficator(username, password))
+                {
+                    Console.WriteLine("--- You've logged into the account ---");
+
+                    
+                }
+                else
+                {
+                    Console.WriteLine("\nIncorrect password or login.\n" +
+                                      "Press enter to retry...");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                }
+            }
+
+        }
+    }
+}
