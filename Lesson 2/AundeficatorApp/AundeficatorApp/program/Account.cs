e4bd433f20992a720eaf57f42c4d3a45ef5af92d
Bogdan Nakonechniy <nopebogdan@gmail.com>
2023-08-23 23:06:23 +0300

    aundifier and account deletion

---
+++ Lesson 2/AundeficatorApp/AundeficatorApp/program/Account.cs
@@ -0,0 +1,33 @@
+﻿using AundeficatorApp.data;
+
+namespace AundeficatorApp.program
+{
+    internal class Account
+    {
+        public static bool Aundeficator(string username, string password)
+        {
+            foreach(var person in db.userArray)
+            {
+                if(person != null && person.GetUsername() == username && person.GetPassword() == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool Delete(string username, string password)
+        {
+            int count = 0;
+            foreach (var person in db.userArray)
+            {
+                if (person.GetUsername() == username && person.GetPassword() == password)
+                {
+                    db.userArray[count] = null;
+                    return true;
+                }
+                count++;
+            }
+            return false;
+        }
+    }
+}
