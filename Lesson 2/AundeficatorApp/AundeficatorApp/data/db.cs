e4a341c0b5db450cdacd6a4ec187e445206db5c0
Bogdan Nakonechniy <nopebogdan@gmail.com>
2023-08-23 23:03:00 +0300

    application database

---
+++ Lesson 2/AundeficatorApp/AundeficatorApp/data/db.cs
@@ -0,0 +1,44 @@
+﻿namespace AundeficatorApp.data
+{
+    internal class db
+    {
+        public static void addUser(string username, string password)
+        {
+            int count = 0;
+            foreach (var user in db.userArray)
+            {
+                if (user == null)
+                {
+                    db.userArray[count] = new User(username, password);
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+
+            }
+        }
+        public static User[] userArray = new User[10];
+    }
+    internal class User
+    {
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+        
+        private string username;
+        private string password;
+
+        public string GetUsername()
+        {
+            return username;
+        }
+        public string GetPassword()
+        {
+            return password;
+        }
+    }
+}
