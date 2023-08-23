08bdcaa94e2d332e56981119cb8881f8cde19e41
Bogdan Nakonechniy <nopebogdan@gmail.com>
2023-08-23 23:09:37 +0300

    interface functions

---
+++ Lesson 2/AundeficatorApp/AundeficatorApp/program/service.cs
@@ -0,0 +1,13 @@
+﻿using AundeficatorApp.data;
+
+namespace AundeficatorApp.program
+{
+    internal static class service
+    {
+        public static void reg(string username, string password)
+        {
+            db.addUser(username, password);
+            Console.WriteLine("Successful registration!");
+        }
+    }
+}
